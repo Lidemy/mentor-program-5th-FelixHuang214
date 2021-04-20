2898d746ef5fcdd8be78d132467831ef8fe7633a
FelixHuang <david021484@gmail.com>
2021-04-20 21:24:13 +0800

    update num.sh

--- homeworks/week1/num.sh
+++ homeworks/week1/num.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-
-for ((i=1;i<=$1;i++))
-do 
-	touch "$i.js"	
-done
+if [ $1 -gt 0 ]; then
+	for ((i=1;i<=$1;i++)); do
+		touch "$i.js"
+	done
 echo "檔案建立完成"	
+else
+	echo "錯誤，請輸入大於 0 的數字"
+fi
